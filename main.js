2297d6ce13eb6e9756d7eda695fe9117cbf11c5e
Javar2022 <120763631+Javar2022@users.noreply.github.com>
2023-04-04 15:40:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-console.log("Testing");
-function yolo () {console.log("Hello");}
+
+var Canvas = (function () {
+  var obj = function () {
+    this.ctx = null;
+  };
+  obj.prototype = {
+    init : function () {
+      var canvas = document.createElement("canvas");
+      canvas.setAttribute("id", "render");
+      canvas.width = "256px";
+      canvas.height = "256px";
+      document.querySelector("body").appendChild(canvas);
+      this.ctx = document.getElementById("render");
+    },
+    setSize : function (width, height) {
+      var canvas = this.ctx !== null ? this.ctx : alert("Cannot set dimensions of uninitialized canvas");
+      canvas.width = width.toString() + "px";
+      canvas.height = height.toString() + "px";
+    }
+  };
+  
+  return Canvas;
+})();
+
